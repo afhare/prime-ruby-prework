8078e9938142ac26f15ba635526d1075277685f1
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 17:35:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(number)
-  if (number <= 0 || number = 1)
-    FALSE
-  end
+  if number <= 1
+  return false
 
-    
+  else (2..number-1).to_a.all? do |integer|
+   number % integer != 0
+
+   end
+  end
 end
