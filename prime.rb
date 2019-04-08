d415bceaefb0be556a97b0fd1d9b8dfe3cfccfc3
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 17:22:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,3 +5,27 @@ def prime?(number)
     FALSE
   end
 end
+
+def prime?(number)
+
+   divisor = 1
+  times = 0
+
+   while divisor <= number
+    if number % divisor == 0
+      times += 1
+    end
+
+     if times > 2
+      break
+    end
+
+     divisor += 1
+  end
+
+   if times == 2
+    true
+  else
+    false
+  end
+end
